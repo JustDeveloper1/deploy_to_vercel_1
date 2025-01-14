fcbd8409e32456eb98575bede71c315b71102c0e
JustDeveloper <justdeveloper@juststudio.is-a.dev>
2025-01-14 09:08:28 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,14 +2,9 @@ import { createMDX } from 'fumadocs-mdx/next';
 
 const withMDX = createMDX();
 
-try {
-  /** @type {import('next').NextConfig} */
-  const config = {
+/** @type {import('next').NextConfig} */
+const config = {
     reactStrictMode: true,
-  };
-
-  export default withMDX(config);
-} catch (error) {
-  console.error("An error occurred while creating MDX configuration:", error);
-  throw new Error("MDX configuration failed");
-}
+};
+  
+export default withMDX(config);

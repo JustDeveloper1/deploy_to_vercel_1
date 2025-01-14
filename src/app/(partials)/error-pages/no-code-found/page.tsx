fbbed08fa40c14ab3420ad3143694323ee76bff7
JustDeveloper <176615419+JustDeveloper1@users.noreply.github.com>
2025-01-14 11:34:06 +0500

    updates

--- src/app/(partials)/error-pages/no-code-found/page.tsx
+++ src/app/(partials)/error-pages/no-code-found/page.tsx
@@ -1,7 +1,7 @@
 export default function NotFoundPage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
-      404 - No Paste Found.
+      404 - No Code Found.
     </main>
   );
 }

a3ae867a714b47a53e4447916fa5417d30f457a6
JustDeveloper <justdeveloper@juststudio.is-a.dev>
2025-01-17 21:36:59 +0300

    Update params.ts

--- src/app/(page)/code/[id]/params.ts
+++ src/app/(page)/code/[id]/params.ts
@@ -1,8 +1,3 @@
 export async function generateStaticParams() {
-  const response = await fetch('https://api.juststudio.is-a.dev/cs');
-  const codes = await response.json();
-  
-  return {}/*codes.map((code: { id: string }) => ({
-    id: code.id.toString(),
-  }));*/
+  return {[{id:"1"}]}
 }

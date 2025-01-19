de8e668c7c89e1b8ac6aa5f426e205be86cdc031
JustDeveloper <justdeveloper@juststudio.is-a.dev>
2025-01-19 23:39:46 +0300

    Update route.ts

--- src/app/(page)/js/main.js/route.ts
+++ src/app/(page)/js/main.js/route.ts
@@ -1,27 +1,11 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import * as fs from 'fs/promises';
-
-const filename = 'main'
-
-async function getFileContent() {
-    try {
-        const content = await fs.readFile(`/js/${filename}.js/data.js`, 'utf8');
-        return content;
-    } catch (error) {
-        console.error('Error reading file:', error);
-        return false;
-    }
-}
 
 export async function GET(
   request: Request,
 ) {
-  const fileContent = await getFileContent();
-  if (fileContent)
-    return new Response(fileContent, {
-      status: 200,
-    });
-  return new Response(null, {
-    status: 404,
+  return new Response(`
+    console.log('a');
+  `, {
+    status: 200,
   });
 }

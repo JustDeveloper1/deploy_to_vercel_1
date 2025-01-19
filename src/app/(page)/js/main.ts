cb29bc1ab501c2dc2d43bc2d181691de452de7cc
JustDeveloper <justdeveloper@juststudio.is-a.dev>
2025-01-19 21:16:48 +0300

    Update main.ts

--- src/app/(page)/js/main.ts
+++ src/app/(page)/js/main.ts
@@ -1,9 +1,38 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
+import * as fs from 'fs';
+
+const filename = 'main'
+
+async function getFileContent() {
+    try {
+        let content;
+        fs.readFile(`../scripts/js/${filename}.js`, 'utf8', (err, data) => {
+            if (err) {
+                console.error('Error reading file:', err);
+                return;
+            }
+            content = data;
+        });
+        return content;
+    } catch (error) {
+        console.error('Error reading file:', error);
+    }
+}
+
+const fileContent = getFileContent();
+
+type ResponseData = {
+  message: string
+}
  
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   res.setHeader('Content-Type', 'application/javascript');
-  res.status(200).send(`console.log('a')`);
+  if (fileContent) {
+    res.status(200).send(fileContent);
+  } else {
+    res.status(404);
+  }
 }

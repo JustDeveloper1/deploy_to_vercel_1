53680fac84fbb0d5e31800b8ff7a0625a88d9f78
JustDeveloper <176615419+JustDeveloper1@users.noreply.github.com>
2025-01-19 16:59:26 +0500

    upd

--- src/lib/db/scripts/crons/paste-expires-check.sql
+++
@@ -1,7 +0,0 @@
--- Cron Job name cannot be edited
-select
-  cron.schedule (
-    'paste-expires-check',
-    '0 */6 * * *',
-    '/* This Cron job will check if any paste is expired. */ SELECT net.http_post(url := ''{{APP_URL}}/api/server/paste-expires-check'', headers := JSONB_BUILD_OBJECT(''Authorization'', ''{{REST_API_PASSWORD}}'') /* Don''t share this Authorization Key. */, timeout_milliseconds := 10000);'
-  );

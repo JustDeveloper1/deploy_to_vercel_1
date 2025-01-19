eb9f3c0b58db99f8b77756f9925aabd8989b2db5
JustDeveloper <justdeveloper@juststudio.is-a.dev>
2025-01-19 15:40:32 +0300

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,9 @@
 import { NextRequest } from "next/server";
+import { createClient } from "@/lib/supabase/middleware";
+
+export async function middleware(request: NextRequest) {
+	return createClient(request);
+}
 
 export const config = {
 	matcher:

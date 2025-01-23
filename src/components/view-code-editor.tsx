250975cf0f80b8451008897b3c836780000c4db0
JustDeveloper <justdeveloper@juststudio.is-a.dev>
2025-01-23 21:28:58 +0300

    Update view-code-editor.tsx

--- src/components/view-code-editor.tsx
+++ src/components/view-code-editor.tsx
@@ -94,7 +94,7 @@ export function ViewCodeEditor({
             ) : (
               <>
                 <Button asChild>
-                  <Link href={`https://api.juststudio.is-a.dev/cs/${id}`}>
+                  <Link href={`/${id}/raw`}>
                     <ScrollText size={16} className="size-2 sm:size-4" />
                     View Raw
                   </Link>

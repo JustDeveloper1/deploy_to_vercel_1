69bb2dfa8bb0f6ea4c2b9d3379cfb60cf8323aba
JustDeveloper <justdeveloper@juststudio.is-a.dev>
2025-01-24 20:53:59 +0300

    Update gtag.tsx

--- src/config/gtag.tsx
+++ src/config/gtag.tsx
@@ -1,7 +1,7 @@
-<head>
+return(
   {/* Google tag (gtag.js) */}
   <Script async src="https://www.googletagmanager.com/gtag/js?id=G-CRFMGJ9Y7D" />
   <Script> 
     {`window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-CRFMGJ9Y7D');`}
   </Script>
-</head>
+)

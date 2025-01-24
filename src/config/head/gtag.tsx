2e5be46e918fe0ae9d464674496ee911e5d41f1a
JustDeveloper <justdeveloper@juststudio.is-a.dev>
2025-01-24 21:21:23 +0300

    Update gtag.tsx

--- src/config/head/gtag.tsx
+++ src/config/head/gtag.tsx
@@ -2,11 +2,13 @@ import Script from 'next/script';
 
 function GoogleTag() {
   return(
-    {/* Google tag (gtag.js) */}
-    <Script async src="https://www.googletagmanager.com/gtag/js?id=G-CRFMGJ9Y7D" />
-    <Script> 
-      {`window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-CRFMGJ9Y7D');`}
-    </Script>
+    <>
+      {/* Google tag (gtag.js) */}
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-CRFMGJ9Y7D" />
+      <Script> 
+        {`window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-CRFMGJ9Y7D');`}
+      </Script>
+    </>
   )
 }
 

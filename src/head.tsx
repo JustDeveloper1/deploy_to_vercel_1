8b5bfe116c42937c2c703acb7f4638479f6fbfcd
JustDeveloper <justdeveloper@juststudio.is-a.dev>
2025-01-25 09:51:04 +0300

    Update head.tsx

--- src/head.tsx
+++ src/head.tsx
@@ -3,7 +3,6 @@ import { GoogleTag } from "@/config/head/gtag";
 function HeadInnerHTML() {
   return (
     <>
-      <link rel="stylesheet" type="text/css" href="/css/style.css" />
       <GoogleTag />
     </>
   )

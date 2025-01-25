78ee47d01680bb5458191257e1429b2a783f8727
JustDeveloper <justdeveloper@juststudio.is-a.dev>
2025-01-25 23:19:08 +0300

    Update page.tsx

--- src/app/(page)/settings/page.tsx
+++ src/app/(page)/settings/page.tsx
@@ -12,6 +12,7 @@ export default function SettingsPage() {
         <header>
           <p className="text-3xl font-semibold">Settings</p>
         </header>
+        
         <section className="space-y-2">
           <div>
             <p className="text-lg font-medium">Appearance</p>
@@ -104,6 +105,72 @@ export default function SettingsPage() {
             </div>
           </Button>
         </section>
+
+        <section className="space-y-2">
+          <div>
+            <p className="text-lg font-medium">Effects</p>
+            <p className="text-sm text-muted-foreground">
+              Enable or disable fresh effects for this website.
+            </p>
+          </div>
+          <Button
+            asChild
+            variant={"ghost"}
+            className="h-fit w-fit"
+            id="_just_effects_enable"
+          >
+            <div className="flex flex-col">
+              <div className="items-center rounded-md border-2 border-muted p-1 hover:border-accent">
+                <div className="space-y-2 rounded-sm bg-[#ecedef] p-2">
+                  <div className="space-y-2 rounded-md bg-white p-2 shadow-sm">
+                    <div className="h-2 w-[80px] animate-pulse rounded-lg bg-[#ecedef]" />
+                    <div className="h-2 w-[100px] animate-pulse rounded-lg bg-[#ecedef]" />
+                  </div>
+                  <div className="flex items-center space-x-2 rounded-md bg-white p-2 shadow-sm">
+                    <div className="h-4 w-4 animate-pulse rounded-full bg-[#ecedef]" />
+                    <div className="h-2 w-[100px] animate-pulse rounded-lg bg-[#ecedef]" />
+                  </div>
+                  <div className="flex items-center space-x-2 rounded-md bg-white p-2 shadow-sm">
+                    <div className="h-4 w-4 animate-pulse rounded-full bg-[#ecedef]" />
+                    <div className="h-2 w-[100px] animate-pulse rounded-lg bg-[#ecedef]" />
+                  </div>
+                </div>
+              </div>
+              <span className="block w-full p-2 text-center font-normal">
+                Enable
+              </span>
+            </div>
+          </Button>
+          <Button
+            asChild
+            variant={"ghost"}
+            id="_just_effects_disable"
+            className="h-fit w-fit"
+          >
+            <div className="flex flex-col">
+              <div className="items-center rounded-md border-2 border-muted bg-popover p-1 hover:bg-accent hover:text-accent-foreground">
+                <div className="space-y-2 rounded-sm bg-neutral-950 p-2">
+                  <div className="space-y-2 rounded-md bg-neutral-800 p-2 shadow-sm">
+                    <div className="h-2 w-[80px] animate-pulse rounded-lg bg-neutral-400" />
+                    <div className="h-2 w-[100px] animate-pulse rounded-lg bg-neutral-400" />
+                  </div>
+                  <div className="flex items-center space-x-2 rounded-md bg-neutral-800 p-2 shadow-sm">
+                    <div className="h-4 w-4 animate-pulse rounded-full bg-neutral-400" />
+                    <div className="h-2 w-[100px] animate-pulse rounded-lg bg-neutral-400" />
+                  </div>
+                  <div className="flex items-center space-x-2 rounded-md bg-neutral-800 p-2 shadow-sm">
+                    <div className="h-4 w-4 animate-pulse rounded-full bg-neutral-400" />
+                    <div className="h-2 w-[100px] animate-pulse rounded-lg bg-neutral-400" />
+                  </div>
+                </div>
+              </div>
+              <span className="block w-full p-2 text-center font-normal">
+                Disable
+              </span>
+            </div>
+          </Button>
+        </section>
+        
       </div>
     </main>
   );

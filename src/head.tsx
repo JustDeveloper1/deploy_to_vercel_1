8e9da7b5f990c6d60519e319aa4ad24bb44c9145
JustDeveloper <justdeveloper@juststudio.is-a.dev>
2025-01-29 11:48:43 +0300

    Update head.tsx

--- src/head.tsx
+++ src/head.tsx
@@ -1,8 +1,14 @@
+"use client";
+
 import { GoogleTag } from "@/config/head/gtag";
 
 function HeadInnerHTML() {
   return (
     <>
+      <link rel="apple-touch-icon" sizes="180x180" href="https://img.juststudio.is-a.dev/cs/ico/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="https://img.juststudio.is-a.dev/cs/ico/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="https://img.juststudio.is-a.dev/cs/ico/favicon-16x16.png" />
+      <link rel="manifest" href="https://img.juststudio.is-a.dev/cs/ico/site.webmanifest" />
       <meta property="og:url" content="https://codeshare.js.org/" />
       <meta property="og:title" content="CodeShare" />
       <meta property="og:site_name" content="CodeShare" />

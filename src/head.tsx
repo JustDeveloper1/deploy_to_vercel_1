8c05198ba98ecc3dcdc44716ff3790fc983de110
JustDeveloper <justdeveloper@juststudio.is-a.dev>
2025-01-29 09:14:43 +0300

    Open Graph

--- src/head.tsx
+++ src/head.tsx
@@ -3,6 +3,13 @@ import { GoogleTag } from "@/config/head/gtag";
 function HeadInnerHTML() {
   return (
     <>
+      <meta property="og:url" content="https://codeshare.js.org/" />
+      <meta property="og:title" content="CodeShare" />
+      <meta property="og:site_name" content="CodeShare" />
+      <meta property="og:description" content="CodeShare is the best tool for storing and sharing codes and text, aka paste tool." />
+      <meta property="twitter:card" content="summary" />
+      <meta property="twitter:title" content="CodeShare" />
+      <meta property="twitter:description" content="CodeShare is the best tool for storing and sharing codes and text, aka paste tool." />
       <GoogleTag />
     </>
   )
